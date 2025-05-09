b56cce13329662da3f3592e494f6d52b93166309
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-05-09 14:21:19 +0300

    feat: add retrieve product through purchase functionality

--- neo4jproduct/retrieveproducts.go
+++ neo4jproduct/retrieveproducts.go
@@ -22,9 +22,34 @@ func RetrieveShopProducts(n *neo4jutils.Neo4jInstance, shopid int) ([]product.Pr
 	var productlist []product.Product
 	// Loop through results and do something with them
 	for _, record := range result.Records {
-		student, _ := record.Get("product") // .Get() 2nd return is whether key is present
+		product, _ := record.Get("product") // .Get() 2nd return is whether key is present
 		var p Product
-		p.Props = student.(neo4j.Node).Props
+		p.Props = product.(neo4j.Node).Props
+		productlist = append(productlist, p)
+	}
+	return productlist, nil
+}
+
+
+// RetrievePurchasesProduct retrieves a product node that belongs to a particular purchase in a Neo4j database using the provided purchaseid and a Neo4jInstance. Returns an error upon failure
+func RetrievePurchaseProduct(n *neo4jutils.Neo4jInstance, purchaseid int) ([]product.Product, error) {
+	result, err := neo4j.ExecuteQuery(n.Ctx, n.Driver,
+		"MATCH (purchase:Purchase {pk: $purchaseid})-[:INVOLVES_PRODUCT]->(product:Product) RETURN product AS product",
+		map[string]any{
+			"purchaseid": purchaseid, // Bind the mapped purchaseid data to the "$purchaseid" parameter
+		},
+		neo4j.EagerResultTransformer,
+		neo4j.ExecuteQueryWithDatabase(n.Db))
+	if err != nil {
+		return nil, err
+	}
+
+	var productlist []product.Product
+	// Loop through results and do something with them
+	for _, record := range result.Records {
+		product, _ := record.Get("product") // .Get() 2nd return is whether key is present
+		var p Product
+		p.Props = product.(neo4j.Node).Props
 		productlist = append(productlist, p)
 	}
 	return productlist, nil
