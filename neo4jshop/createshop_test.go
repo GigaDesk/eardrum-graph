cccfb8d2f6efc4a36fe858482b603e9c0a6c5ea6
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2025-05-12 14:30:28 +0300

    test: test create shop functionality

---
+++ neo4jshop/createshop_test.go
@@ -0,0 +1,44 @@
+package neo4jshop
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/GigaDesk/eardrum-graph/mockshop"
+	"github.com/GigaDesk/eardrum-graph/mockschool"
+	"github.com/GigaDesk/eardrum-graph/neo4jschool"
+	"github.com/GigaDesk/eardrum-graph/neo4jutils"
+	"github.com/joho/godotenv"
+)
+
+var (
+	neo4jInstance neo4jutils.Neo4jInstance
+)
+
+func TestCreateShopNode(t *testing.T) {
+	
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
+
+	neo4jInstance.Init(os.Getenv("NEO4J_DBURI"), os.Getenv("NEO4J_DBUSER"), os.Getenv("NEO4J_DBPASSWORD"))
+	defer neo4jInstance.Driver.Close(neo4jInstance.Ctx)
+	//create a school node of primary key 1
+	neo4jschool.CreateSchool(&neo4jInstance, mockschool.SchoolNode)
+	//create shop nodes of primary keys 3,4,5 and 6 to school node of primary key 1
+	for _,shop := range mockshop.MultipleShopNodes {
+		if err := CreateShop(&neo4jInstance, shop, 1); err != nil {
+			t.Error(`Failed to add a shop node`)
+		}
+		result, error := CheckShop(&neo4jInstance, 5) 
+		if error != nil {
+			log.Fatal("Failed to check shop")
+		}
+		if !result{
+			t.Error(fmt.Sprintf("shop of id %d is not available", shop.GetID()))
+		}
+	}
+}
